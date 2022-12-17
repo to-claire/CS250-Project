7fa49a870dc3909444200d6b8681ed52fc4eca33
Claire <clockwork.cto@gmail.com>
2022-12-17 12:17:41 -0800

    does this work

--- Interface.cpp
+++ Interface.cpp
@@ -26,167 +26,174 @@ void processChoice(CourseList& courseList)
     }
     else
     {
-        cout << "\nEnter a selection: ";
-        char selection;
-        cin >> selection;
-
-        if (selection == '1')
+        char selection = '\0';
+        while (selection != '6')
         {
-            cout << "\nEnter a course number to search: ";
-            int courseNumber;
-            cin >> courseNumber;
-
-            Node* course = courseList.searchCourse(courseNumber);
-
-            while (course == nullptr)
-            {
-                cout << "\nCourse number is not in the list.\n"
-                     << "\t1: Retry \n\t2: Make another selection.\n";
-                cin >> selection;
-
-                while (selection != '1' && selection != '2')
-                {
-                    cout << "\nSelection is invalid.\n"
-                         << "\t1: Retry \n\t2: Make another selection.\n";
-                    cin >> selection;
-                }
-
-                if (selection == '1')
-                {
-                    cout << "\nEnter a course number to search: ";
-                    cin >> courseNumber;
-                    course = courseList.searchCourse(courseNumber);
-                }
-                else
-                {
-                    displayMenu();
-                    processChoice(courseList);
-                }
-            }
-
-            cout << course->getCourse().getPrefix()
-                 << course->getCourse().getCourseNumber() << ", "
-                 << course->getCourse().getCourseName() << ", "
-                 << course->getCourse().getCourseUnits() << " units\n";
-        }
-        else if (selection == '2')
-        {
-            cout << "\nContact the Curriculum Committee "
-                 << "to start the process of adding a course.\n";
-        }
-        else if (selection == '3')
-        {
-            cout << "Enter a course number to delete: ";
-            int courseNumber;
-            cin >> courseNumber;
-
-            Node* course = courseList.searchCourse(courseNumber);
-
-            while (course == nullptr)
-            {
-                cout << "\nCourse number is not in the list.\n"
-                     << "\t1: Retry \n\t2: Make another selection.\n";
-                cin >> selection;
-
-                while (selection != '1' && selection != '2')
-                {
-                    cout << "\nSelection is invalid.\n"
-                         << "\t1: Retry \n\t2: Make another selection.\n";
-                    cin >> selection;
-                }
-
-                if (selection == '1')
-                {
-                    cout << "\nEnter a course number to delete: ";
-                    cin >> courseNumber;
-                    course = courseList.searchCourse(courseNumber);
-                }
-                else
-                {
-                    displayMenu();
-                    processChoice(courseList);
-                }
-            }
-
-            cout << "Confirm the deletion request? \n"
-                    << "Type 'y' to confirm. "
-                    << "If not, please type a different key to exit. " << endl;
+            cout << "\nEnter a selection: ";
             cin >> selection;
 
-            if (selection == 'y')
+            if (selection == '1')
             {
-                courseList.deleteCourse(courseNumber);
-                cout << "Course is successfully deleted." << endl;
-            }
-        }
-        else if (selection == '4')
-        {
-            string courses;
-            courseList.retrieveAllCourses(courses);
-            cout << courses;
-        }
-        else if (selection == '5')
-        {
-            cout << "Enter a course number you like to see the workload for: ";
-            int courseNumber;
-            cin >> courseNumber;
+                cout << "\nEnter a course number to search: ";
+                int courseNumber;
+                cin >> courseNumber;
 
-            Node* course = courseList.searchCourse(courseNumber);
+                Node* course = courseList.searchCourse(courseNumber);
 
-            while (course == nullptr)
-            {
-                cout << "\nCourse number is not in the list.\n"
-                     << "\t1: Retry \n\t2: Make another selection.\n";
-                cin >> selection;
-
-                while (selection != '1' && selection != '2')
+                while (course == nullptr && selection != '2')
                 {
-                    cout << "\nSelection is invalid.\n"
+                    cout << "\nCourse number is not in the list.\n"
                          << "\t1: Retry \n\t2: Make another selection.\n";
+                    cin >> selection;
+
+                    while (selection != '1' && selection != '2')
+                    {
+                        cout << "\nSelection is invalid.\n"
+                             << "\t1: Retry \n\t2: Make another selection.\n";
+                        cin >> selection;
+                    }
+
+                    if (selection == '1')
+                    {
+                        cout << "\nEnter a course number to search: ";
+                        cin >> courseNumber;
+                        course = courseList.searchCourse(courseNumber);
+                    }
+                    else
+                    {
+                        displayMenu();
+                    }
+                }
+
+                if (selection != '2')
+                {
+                    cout << course->getCourse().getPrefix()
+                         << course->getCourse().getCourseNumber() << ", "
+                         << course->getCourse().getCourseName() << ", "
+                         << course->getCourse().getCourseUnits() << " units\n";
+                }
+            }
+            else if (selection == '2')
+            {
+                cout << "\nContact the Curriculum Committee "
+                     << "to start the process of adding a course.\n";
+            }
+            else if (selection == '3')
+            {
+                cout << "Enter a course number to delete: ";
+                int courseNumber;
+                cin >> courseNumber;
+
+                Node* course = courseList.searchCourse(courseNumber);
+
+                while (course == nullptr && selection != '2')
+                {
+                    cout << "\nCourse number is not in the list.\n"
+                         << "\t1: Retry \n\t2: Make another selection.\n";
+                    cin >> selection;
+
+                    while (selection != '1' && selection != '2')
+                    {
+                        cout << "\nSelection is invalid.\n"
+                             << "\t1: Retry \n\t2: Make another selection.\n";
+                        cin >> selection;
+                    }
+
+                    if (selection == '1')
+                    {
+                        cout << "\nEnter a course number to delete: ";
+                        cin >> courseNumber;
+                        course = courseList.searchCourse(courseNumber);
+                    }
+                    else
+                    {
+                        displayMenu();
+                    }
+                }
+
+                if (selection != '2')
+                {
+                    cout << "Confirm the deletion request? \n"
+                         << "Type 'y' to confirm or any key to exit. " << endl;
                     cin >> selection;
                 }
 
-                if (selection == '1')
+                if (selection == 'y')
                 {
-                    cout << "\nEnter a course number you like to see the workload for: ";
-                    cin >> courseNumber;
-                    course = courseList.searchCourse(courseNumber);
+                    courseList.deleteCourse(courseNumber);
+                    cout << "Course is successfully deleted." << endl;
+                }
+            }
+            else if (selection == '4')
+            {
+                string courses;
+                courseList.retrieveAllCourses(courses);
+                cout << courses;
+            }
+            else if (selection == '5')
+            {
+                cout << "Enter a course number you like to see the workload for: ";
+                int courseNumber;
+                cin >> courseNumber;
+
+                Node* course = courseList.searchCourse(courseNumber);
+
+                while (course == nullptr && selection != '2')
+                {
+                    cout << "\nCourse number is not in the list.\n"
+                         << "\t1: Retry \n\t2: Make another selection.\n";
+                    cin >> selection;
+
+                    while (selection != '1' && selection != '2')
+                    {
+                        cout << "\nSelection is invalid.\n"
+                             << "\t1: Retry \n\t2: Make another selection.\n";
+                        cin >> selection;
+                    }
+
+                    if (selection == '1')
+                    {
+                        cout << "\nEnter a course number you like to see the workload for: ";
+                        cin >> courseNumber;
+                        course = courseList.searchCourse(courseNumber);
+                    }
+                    else
+                    {
+                        displayMenu();
+                    }
+                }
+
+                if (selection != '2')
+                {
+                    cout << "Hours required to study for this course each week: "
+                         << course->getCourse().getCourseUnits() * 3 << endl;
+                }
+            }
+            else if (selection == '6')
+            {
+                // Exit
+            }
+            else
+            {
+                cout << "\nSelection is invalid." << endl;
+            }
+
+            if (selection != '6')
+            {
+                cout << "\nWould you like to continue the program? \n"
+                        << "Type 'y' to confirm or any key to exit. " << endl;
+                cin >> selection;
+
+                if (selection == 'y') {
+                    displayMenu();
                 }
                 else
                 {
-                    displayMenu();
-                    processChoice(courseList);
+                    selection = '6';
                 }
             }
-
-            cout << "Hours required to study for this course each week: "
-                 << course->getCourse().getCourseUnits() * 3 << endl;
-        }
-        else if (selection == '6')
-        {
-            cout << "\nThank-you, good-night, and good-bye.\n";
-            exit(0);
-        }
-        else
-        {
-            cout << "\nSelection is invalid. Please try again.";
-            processChoice(courseList);
         }
 
-        cout << "\nWould you like to continue the program? \n"
-                << "Type 'y' to confirm. "
-                << "If not, please type a different key to exit: " << endl;
-        cin >> selection;
-
-        if (selection == 'y')
-        {
-            displayMenu();
-            processChoice(courseList);
-        }
-        else
-        {
-            cout << "\nThank-you, good-night, and good-bye.\n";
-            exit(0);
-        }
+        cout << "\nThank-you, good-night, and good-bye." << endl;
     }
 }
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/claireto/CLionProjects/cs250-project
+# Install script for directory: /Users/claireto/CLionProjects/CS250-Project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/claireto/CLionProjects/cs250-project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/claireto/CLionProjects/CS250-Project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
